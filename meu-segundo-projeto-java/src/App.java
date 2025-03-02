56409d587159d5fb4d4cdc93b6841a8d5c512d3a
Moises Marques <Moisessantos12@hotmail.com>
2025-03-02 11:42:49 -0300

    quarto comando

--- meu-segundo-projeto-java/src/App.java
+++ meu-segundo-projeto-java/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("eu te amo!");
+        System.out.println("meu nome é Moises!");
+        
+        
     }
 }
