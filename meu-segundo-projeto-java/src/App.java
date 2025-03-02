915b4bcf71096e972a172f2b86d95e8b588af449
Moises Marques <Moisessantos12@hotmail.com>
2025-03-02 11:51:33 -0300

    quinto comando

--- meu-segundo-projeto-java/src/App.java
+++ meu-segundo-projeto-java/src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("meu nome é Moises!");
-        
+        System.out.println("Obrigado Jesus!");
+
         
     }
 }
